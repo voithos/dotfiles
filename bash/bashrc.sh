31558d383a95279f2d23af4f5b4ec76fec8d4836
Zaven Muradyan <zaven@skepsi.me>
2015-03-09 01:34:37 -0700

    Update bash_completion script

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -72,8 +72,12 @@ man() {
 }
 
 # enable programmable completion features
-if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
-    . /etc/bash_completion
+if ! shopt -oq posix; then
+    if [ -f /usr/share/bash-completion/bash_completion ]; then
+        . /usr/share/bash-completion/bash_completion
+    elif [ -f /etc/bash_completion ]; then
+        . /etc/bash_completion
+    fi
 fi
 
 # display ls output as weighted
