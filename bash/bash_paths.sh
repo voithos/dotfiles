4eeda323fdb8de374792a3fe42c14aa8d918b5fa
Zaven Muradyan <zaven@skepsi.me>
2017-07-01 14:15:05 -0700

    Add go path config.

--- bash/bash_paths.sh
+++ bash/bash_paths.sh
@@ -1,5 +1,14 @@
 # installed and external projects bin path
-PATH=$PATH:$HOME/.local/bin:$HOME/.bin
+export PATH=$PATH:$HOME/.local/bin:$HOME/.bin
 
 # virtualenv configuration
 export WORKON_HOME=$HOME/.virtualenvs
+
+# non-work-specific
+if [ ! -f ~/.atwork ]; then
+    # GOPATH
+    if command -v go &>/dev/null; then
+        export GOPATH="$HOME/Software/go"
+        export PATH=$PATH:$(go env GOPATH)/bin
+    fi
+fi
