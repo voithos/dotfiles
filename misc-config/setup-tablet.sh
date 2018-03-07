32ba2f4d76ffa66de83cb94cf3560c5c4fcfde3f
Zaven Muradyan <zaven@skepsi.me>
2018-03-06 21:26:35 -0800

    Add automatic tablet configuration.

--- misc-config/setup-tablet.sh
+++ misc-config/setup-tablet.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-# Screen and tablet configuration for the Huion 1060 Plus drawing tablet.
-# This script limits the tablet to only a single monitor, in order to make the
-# most use of its drawing space, and also modifies some of the pad and stylus
-# buttons.
+# Screen and tablet configuration for the Wacom Cintiq Pro 13.
+# This script limits the tablet touch input to only a single monitor.
 
-set -e
+on_error() {
+  notify-send "Tablet configuration failed; error on line $1"
+}
+trap 'on_error $LINENO' ERR
 
-# Limit tablet to work only on a single monitor.
-MONITOR="DVI-I-1"
-pen_input=$(xinput | grep -e 'Huion.*Pen.*stylus' | cut -f 2 | cut -c 4-)
-xinput map-to-output $pen_input $MONITOR
+# Limit tablet touch to work only on a single monitor.
+TABLET="Cintiq Pro 13"
+MONITOR="HDMI-0"
 
-# Setup stylus button mapping (switch pan/right-click).
-xsetwacom --set 'Huion Tablet HA60-F400 Pen stylus' Button 2 "button +3"
-xsetwacom --set 'Huion Tablet HA60-F400 Pen stylus' Button 3 "button +2"
-
-# Setup pad button mappings.
-xsetwacom --set 'Huion Tablet HA60-F400 Pad pad' Button 1 "key +ctrl +z -z -ctrl"
-xsetwacom --set 'Huion Tablet HA60-F400 Pad pad' Button 2 "key +ctrl +shift +z -z -shift -ctrl"
-xsetwacom --set 'Huion Tablet HA60-F400 Pad pad' Button 3 "key e"
+finger_touch=$(xinput | grep -e "$TABLET.*Touch" | cut -f 2 | cut -c 4-)
+if [[ $finger_touch ]]; then
+  xinput map-to-output $finger_touch $MONITOR
+fi
 
 notify-send "Tablet configured"
