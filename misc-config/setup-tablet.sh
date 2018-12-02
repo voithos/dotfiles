cf353e5b3e70df4b2ec2e1d34fec7551062c12a9
Zaven Muradyan <zaven@voithos.io>
2018-12-01 23:08:51 -0800

    Add automatic Ctrl+Z mapping to stylus button 3.

--- misc-config/setup-tablet.sh
+++ misc-config/setup-tablet.sh
@@ -17,4 +17,10 @@ if [[ $finger_touch ]]; then
   xinput map-to-output $finger_touch $MONITOR
 fi
 
+# Map pen button 3 to "Ctrl+Z".
+stylus=$(xinput | grep -e "$TABLET.*Pen stylus" | cut -f 2 | cut -c 4-)
+if [[ $stylus ]]; then
+  xsetwacom set $stylus Button 3 "KEY ctrl + z"
+fi
+
 notify-send "Tablet configured"
