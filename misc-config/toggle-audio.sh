6ce238dbf5c840cd0ff5c5174ff7ae2a7b5f248d
Zaven Muradyan <zaven@voithos.io>
2021-06-27 13:58:10 -0700

    Fix toggle-audio.sh.

--- misc-config/toggle-audio.sh
+++ misc-config/toggle-audio.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Script that switches from finicky bluetooth headphones to system audio, and
-# back. Requires the use of a2dp.py to avoid headset audio issues.
+# Script that switches from headphones to system audio, and back.
 
-headphones_index=$(pacmd list-sinks | egrep -v "pci" | grep -B 1 "name:" | tr "\n" ":" | tr -d "[[:space:]]" | tr -d "^\*" | cut -f 2 -d ":")
-speaker_index=$(pacmd list-sinks | egrep -v "usb|pci.*extra" | grep -B 1 "name:" | tr "\n" ":" | tr -d "[[:space:]]" | tr -d "^\*" | cut -f 2 -d ":")
+headphones_index=$(pacmd list-sinks | grep -B 1 "name:.*Razer" | tr "\n" ":" | tr -d "[[:space:]]" | tr -d "^\*" | cut -f 2 -d ":")
+speaker_index=$(pacmd list-sinks | grep -B 1 "name:.*TX-Hifi" | tr "\n" ":" | tr -d "[[:space:]]" | tr -d "^\*" | cut -f 2 -d ":")
 current_index=$(pacmd list-sinks | grep "\*.*index:" | tr "\n" ":" | tr -d "[[:space:]]" | tr -d "^\*" | cut -f 2 -d ":")
 
 echo "Headphones index: $headphones_index"
@@ -26,18 +25,11 @@ if [ "$headphones_index" = "$current_index" ]; then
         done
         pacmd set-default-sink $new_index
     fi
-    notify-send "Switched to speakers"
+    notify-send --hint=int:transient:1 -t 500 -u normal "Switched to speakers"
 elif [ "$speaker_index" = "$current_index" ]; then
     echo "Switching to headphones"
 
-    # Using bluetooth headphones has some buggy subtleties; call a Python
-    # script to set this up. Assume it's in the same directory as the script.
-    # First, we need the MAC address of our headphones.
-    mac_address=$(pacmd list-sinks | sed -e "/index: $headphones_index/,/device.string/!d" | grep 'device.string' | grep -o '".*"' | tr -d '"')
-    $(dirname $0)/a2dp.py "$mac_address"
-
-    # Grab the headphones index again, in case it has changed.
-    new_index=$(pacmd list-sinks | egrep -v "pci" | grep -B 1 "name:" | tr "\n" ":" | tr -d "[[:space:]]" | tr -d "^\*" | cut -f 2 -d ":")
+    new_index=$headphones_index
     playback_streams=$(pacmd list-sink-inputs | grep "index" | cut -c 12-)
     readarray -t playback_streams <<<"$playback_streams"
 
@@ -47,7 +39,7 @@ elif [ "$speaker_index" = "$current_index" ]; then
         done
     fi
     pacmd set-default-sink $new_index
-    notify-send "Switched to headphones"
+    notify-send --hint=int:transient:1 -t 500 -u normal "Switched to headphones"
 else
     notify-send "Unknown current playback device index! '$current_index'"
     exit
